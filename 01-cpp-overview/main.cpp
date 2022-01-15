b69c98864aa66b5c795376e73d3435b9cee25601
Benjamin Arsenault <benarsenaultdev@gmail.com>
2022-01-14 21:29:54 -0500

    Initial base commit

---
+++ 01-cpp-overview/main.cpp
@@ -0,0 +1,27 @@
+// 1.1 Your first C++ program
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "hello, world" << endl;
+	return 0;
+}
+
+
+// 1.2 The history of C++
+// - In 155, this IBM team produced the initial version of FORTRAN (whose name is a contraction of formula translation), which was the first example of a higher-level programming language.
+// - In programming, the old order is represented by the procedural paradigm, in which programs consist of a collection of procedures and functions that operate on data.
+// - The new model is called the object-oriented paradigm, in which programs are viewed instead as a collection of data objects that embody particular characteristics and behavior.
+// - As a programmer, you must master many different paradigms, so that you can use the model that is most appropriate to the task at hand.
+
+
+// 1.3 The compilation process
+// - Source file needs to be translated into executable form.
+// - The compiler program translates the source file into an object file containg the machine-language instructions.
+// - This object file is then combined with other object files to produce an executable file.
+// - The other object files typically included predefined object files called libraries, which contain the machine-language instructions for various operations commonly required by programs.
+// - The process of combining all the individual object files into an executable file is called linking.
+
+
+// 1.4 The structure of a C++ program
+// - 
