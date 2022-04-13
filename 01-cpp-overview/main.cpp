9e75eb00d6ee6160d9702ae8d09723eeee27288a
Benjamin Arsenault <hi@benjamin.dev>
2022-04-12 20:25:49 -0400

    Finish 1.5 notes

--- 01-cpp-overview/main.cpp
+++ 01-cpp-overview/main.cpp
@@ -9,19 +9,19 @@ int main() {
 
 
 /* 1.2 The history of C++
- - In 155, this IBM team produced the initial version of FORTRAN (whose name is a contraction of formula translation), which was the first example of a higher-level programming language.
- - In programming, the old order is represented by the procedural paradigm, in which programs consist of a collection of procedures and functions that operate on data.
- - The new model is called the object-oriented paradigm, in which programs are viewed instead as a collection of data objects that embody particular characteristics and behavior.
- - As a programmer, you must master many different paradigms, so that you can use the model that is most appropriate to the task at hand.
+ - in 155, this IBM team produced the initial version of FORTRAN (whose name is a contraction of formula translation), which was the first example of a higher-level programming language.
+ - in programming, the old order is represented by the procedural paradigm, in which programs consist of a collection of procedures and functions that operate on data.
+ - the new model is called the object-oriented paradigm, in which programs are viewed instead as a collection of data objects that embody particular characteristics and behavior.
+ - as a programmer, you must master many different paradigms, so that you can use the model that is most appropriate to the task at hand.
 */
 
 
 /* 1.3 The compilation process
- - Source file needs to be translated into executable form.
- - The compiler program translates the source file into an object file containg the machine-language instructions.
- - This object file is then combined with other object files to produce an executable file.
- - The other object files typically included predefined object files called libraries, which contain the machine-language instructions for various operations commonly required by programs.
- - The process of combining all the individual object files into an executable file is called linking.
+ - source file needs to be translated into executable form.
+ - the compiler program translates the source file into an object file containg the machine-language instructions.
+ - this object file is then combined with other object files to produce an executable file.
+ - the other object files typically included predefined object files called libraries, which contain the machine-language instructions for various operations commonly required by programs.
+ - the process of combining all the individual object files into an executable file is called linking.
 */
 
 
@@ -35,5 +35,15 @@ int main() {
 
 
 /* 1.5 Variables
+ - data values in a program are usually stored in variables, which are named locations in memory capable of holding a particular data type.
+ type namelist;
+ int limit;
+ double n1, n2, n3;
+ int result = 1;
+ - an initial value specified as part of a declaration is called an initializer. 
+ const double PI = 3.14
+*/
+
+/* 1.6 Data types
  - 
 */
